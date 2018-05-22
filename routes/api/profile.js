0836c7ead05bd8535085c6f023cb7ff83cdb49db
peterlxb <peterlxb@gmail.com>
2018-05-22 16:15:07 +0800

    Add profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,6 +39,60 @@ router.get(
   }
 );
 
+//@route GET api/profile/all
+//@desc Get  all profiles
+//@access Public
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate({ model: "users", path: "user", select: ["name", "avatar"] })
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There res no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There ars no profiles " }));
+});
+
+//@route GET api/profile/handle/:handle
+//@desc Get profile by handle
+//@access Public
+router.get("/handle/:handle", (req, res) => {
+  Profile.findOne({ handle: req.params.handle })
+    .populate({ model: "users", path: "user", select: ["name", "avatar"] })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access Public
+router.get("/user/:user_id", (req, res) => {
+  Profile.findOne({ user: req.params.user_id })
+    .populate({ model: "users", path: "user", select: ["name", "avatar"] })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
 //@route POST api/profile/
 //@desc Create user profile
 //@access Private
