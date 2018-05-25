5583dc50e3e2d53b12599c9c5008432b9eee116c
peterlxb <peterlxb@gmail.com>
2018-05-25 15:08:00 +0800

    Add like & unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -74,12 +74,83 @@ router.delete(
       .then(profile => {
         Post.findById(req.params.id).then(post => {
           // Check for post owner
+          console.log(post);
           if (post.user.toString() !== req.user.id) {
             return res.status(401).json({ noauth: "user no auth" });
           }
 
           // Delete
           post.remove().then(() => res.json({ success: true }));
+        });
+      })
+      .catch(err => res.status(404).json({ posts: "Post not found!" }));
+  }
+);
+
+//@route POST api/posts/like/:id
+//@desc like post
+//@access Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        console.log(req.params.id);
+        Post.findById(req.params.id).then(post => {
+          if (!post) {
+            return res.status(404).json({ error: "Post not exist" });
+          }
+
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreayLiked: "User already liked this post" });
+          }
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        });
+      })
+      .catch(err => res.status(404).json({ posts: "Post not found!" }));
+  }
+);
+
+//@route POST api/posts/unlike/:id
+//@desc Unlike post
+//@access Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        console.log(req.params.id);
+        Post.findById(req.params.id).then(post => {
+          if (!post) {
+            return res.status(404).json({ error: "Post not exist" });
+          }
+
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notLiked: "You have not liked this post" });
+          }
+          // get the remove index of array
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice the array
+          post.likes.splice(removeIndex, 1);
+          post.save().then(post => res.json(post));
         });
       })
       .catch(err => res.status(404).json({ posts: "Post not found!" }));
