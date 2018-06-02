0820d9683441e18d536931f3032d674c733bde02
peterlxb <peterlxb@gmail.com>
2018-06-02 20:09:21 +0800

    Add Education Form && Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentiala/AddExperience";
+import AddEducation from "./components/add-credentiala/AddEducation";
 import store from "./store";
 import "./App.css";
 
@@ -68,6 +69,11 @@ class App extends Component {
                 exact
                 path="/add-experience"
                 component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </div>
