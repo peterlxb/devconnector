5f3fd0aaee6e03b42f29e2d86f5b00cc1a3304eb
peterlxb <peterlxb@gmail.com>
2018-06-04 21:29:22 +0800

    Add keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://peter:peter@ds227110.mlab.com:27110/devconnector-01",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
